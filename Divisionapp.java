c2b5efb3aa61f5a8e57123385e84bc910293382c
LUCY-23-STACK <2300713133@mubs.ac.ug>
2024-10-04 16:33:50 +0100

    first commit

---
+++ Divisionapp.java
@@ -0,0 +1,35 @@
+//package divisionapp;
+//
+//public class Divisionapp {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
+
+public class Divisionapp {
+    public static void main(String[] args) {
+        int number = 24;
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println("The number is Even.");
+            // Nested if to check divisibility by 6
+//            if (number % 6 == 0) {
+//                System.out.println("And it is divisible by 6.");
+//            } else {
+//                System.out.println("And it is not divisible by 6.");
+//            }
+        } else {
+            System.out.println("The number is Odd.");
+            // Nested if to check divisibility by 3
+//            if (number % 3 == 0) {
+//                System.out.println("And it is divisible by 3.");
+//            } else {
+//                System.out.println("And it is not divisible by 3.");
+//            }
+        }
+    }
+}
